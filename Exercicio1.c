4f8fa7f637d7e571f9539b24625203135ec78405
Dollonaro <lucasbelia91@gmail.com>
2024-09-17 21:29:09 -0300

    ra

--- Exercicio1.c
+++ Exercicio1.c
@@ -1,4 +1,4 @@
 //Cauã Souza Dias - RA: 1988941
-//João Pedro Ramos Ferreira - RA: 
+//João Pedro Ramos Ferreira - RA: 1993757
 //Lucas Belia Canguçu Strambaioli - RA: 1996497
-//Rian Moraes Carvalho - RA: 
+//Rian Moraes Carvalho - RA: 2001292
