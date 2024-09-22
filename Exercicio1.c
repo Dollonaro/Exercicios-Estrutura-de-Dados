3f3eb01eb98d157cb3f440444b4720e2cca990aa
Dollonaro <lucasbelia91@gmail.com>
2024-09-22 10:56:43 -0300

    funçoes comentadas

--- Exercicio1.c
+++ Exercicio1.c
@@ -4,12 +4,15 @@
 //Rian Moraes Carvalho - RA: 2001292
 
 #include <stdio.h>
+#include <locale.h>
 
+//Criação de um novo tipo de dados para possibilitar o retorno na função tabuada
 typedef struct{
-    int vetor[10]
+    int vetor[10];
 } t_tabuada;
 
 
+//Função que recebe um valor inteiro, multiplica ele por valores de 1 a 10 e retorna o vetor com esses valores multiplicados
 t_tabuada tabuada(int N){
     int i;
     t_tabuada vetorTabuada;
@@ -21,20 +24,22 @@ t_tabuada tabuada(int N){
 }
 
 
+//Função que recebe um valor inteiro e verifica se ele é par, retornando 1 nos pares e 0 nos ímpares   
 int eh_par(int x){
     if(x%2==0){
-        return 0;
-    } else{
         return 1;
+    } else{
+        return 0;
     }
 }
 
 
+//Função que recebe uma matriz[4][5] de valores inteiros e utiliza da função eh_par para verificar a quantidade de pares presentes nela, depois retorna essa quantidade
 int paresDaMatriz(int matriz[4][5]){
     int i,j,quantPar=0;
     for(i=0; i<4; i++){
-        for(j=0; i<5; i++){
-            if(eh_par(matriz[i][j]) == 0){
+        for(j=0; j<5; j++){
+            if(eh_par(matriz[i][j]) == 1){
                 quantPar+=1;
             }
         }
@@ -45,25 +50,31 @@ int paresDaMatriz(int matriz[4][5]){
 
 
 void main(){
-    int i, j, m[4][5], valor;
-    //printf("Digite um valor: ");
-    //scanf("%d", &valor);
-    //for(i=0;i<10;i++){
-    //    printf("%d\n", tabuada(valor).vetor[i]);
-    //}
-    //fflush(stdin);
+    setlocale(LC_ALL, "Portuguese");
+
+
+    int i, j, m[4][5], valor, valor_matriz;
+    printf("Digite um valor: ");
+    scanf("%d", &valor);
+    for(i=0;i<10;i++){
+        printf("%d\n", tabuada(valor).vetor[i]);
+    }
+    
     
 
     for(i=0;i<4;i++){
         for(j=0;j<5;j++){
-           m[i][j]=j;
+            printf("Digite um valor para a posicao [%d][%d]",i,j);
+            scanf("%d", &valor_matriz);
+           m[i][j]=valor_matriz;
         }
     }
     for(i=0;i<4;i++){
+        printf("\n");
         for(j=0;j<5;j++){
-           printf("%d",m[i][j]);
+           printf("%d ",m[i][j]);
         }
     }
-    printf("A quantidade de pares nessa matriz eh %d", paresDaMatriz(m));
-    fflush(stdin);
+    printf("\nA quantidade de pares nessa matriz eh %d", paresDaMatriz(m));
+
 }
